8023bf32b424cc547aff4deaf0213a9ddd727dfc
Piyushrathoree <meonpath008@gmail.com>
2025-03-03 20:01:51 +0530

    Complete TeamSpace model schema with project details and references

--- server/models/teamSpace.model.js
+++ server/models/teamSpace.model.js
@@ -1,5 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 const teamSpaceSchema = new mongoose.Schema({
-    
+    projectName:{
+        type: String,
+        required: true
+    },
+    OwnerId:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    members:[{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+    }],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
 })
+
+const TeamSpace = mongoose.model('TeamSpace', teamSpaceSchema);
