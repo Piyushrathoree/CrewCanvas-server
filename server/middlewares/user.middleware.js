ff8db09cb29c92b5bc13a5cdb45f5e4451194181
Piyushrathoree <meonpath008@gmail.com>
2025-03-08 02:20:02 +0530

    middleware done
    Co-authored-by: VedangKanade <VedangKanade20@users.noreply.github.com>

---
+++ server/middlewares/user.middleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+const userMiddleware = async (req, _, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded._id).select("-password");
+        req.user = user;
+        next();
+    } catch (err) {
+        console.error(err);
+        throw new Error("Not authorized, token failed");
+    }
+};
+
+export default userMiddleware;
