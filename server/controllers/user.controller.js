d11dabc03bc2bc36a60502876e9eed7b66208760
Piyushrathoree <meonpath008@gmail.com>
2025-03-16 03:38:29 +0530

    Refactor user authentication controller: enhance registration, login, email verification, and password reset functionalities

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,77 +1,183 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import { generateVerificationCode } from "../utils/generateToken.js";
+import {
+    sendVerificationEmail,
+    sendWelcomeEmail,
+    sendPasswordResetEmail,
+    sendResetSuccessfulEmail,
+} from "../mail/emails.js";
 
-const RegisterUser = async (req, res) => {
-    const { email, name, password, picture } = req.body;
-
-    const userExists = await User.findOne({ email });
-
-    if (userExists) {
-        res.status(400);
-        throw new Error({ message: "User already exists" });
+export const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        return res.status(400).send("Please fill all the fields");
     }
+
+    let existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(401).send("User already exists");
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const verificationToken = generateVerificationCode();
 
     const user = new User({
-        name: { firstName: name.firstName, lastName: name.lastName || "" },
+        name,
         email,
-        picture,
-        password:await User.hashPassword(password),
+        password: hashPassword,
+        verificationToken,
+        verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
     });
 
-    if (!user) {
-        return res.status(404).json({ message: "Register failed, Try again" });
-    }
+    // directly authenticating user
+    const token = await User.generateAuthToken();
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        sameSite: "strict",
+    });
+
+    await sendVerificationEmail(user.email, verificationToken);
     await user.save();
-    const finalUser = await User.findById(user._id).select("-password"); // go for proper reference
-    res.status(201).json({ message: "Registered successfully", finalUser });
+    res.status(200).json({
+        message: "User registered successfully",
+        user: {
+            ...user._doc,
+            password: undefined,
+        },
+    });
 };
 
-const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-        return res
-            .status(400)
-            .json({ message: "Email and password are required" });
-    }
-
-    const user = await User.findOne({ email });
-    if (!user) {
-        return res
-            .status(404)
-            .json({ message: "User not found, please register" });
-    }
-
+export const loginUser = async (req, res) => {
     try {
-        const isPasswordCorrect = await user.isPasswordCorrect(password);
-        console.log("Password comparison result:", isPasswordCorrect);
+        const { email, password } = req.body;
+        const user = await User.findOne({ email }).select("+password");
 
-        if (!isPasswordCorrect) {
-            return res
-                .status(401)
-                .json({ message: "Wrong password, please try again" });
+        if (!user || !(await user.isPasswordCorrect(password))) {
+            return res.status(401).json({ message: "Invalid credentials" });
         }
+        const token = await User.generateAuthToken();
 
-        const token = user.generateToken();
-        const loggedUser = await User.findById(user._id).select("-password");
-
+        user.lastLogin = new Date();
+        user.save();
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            sameSite: "strict",
+        });
         res.status(200).json({
-            message: "Login successful",
+            user: {
+                ...user._doc,
+                password: undefined,
+            },
             token,
-            loggedUser,
+            message: "User logged in successfully",
         });
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: "Internal server error" });
     }
 };
 
-const logOut = async (req, res) => {
-    const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(' ')[1] || req.cookies.token;
-    if (!token) {
-        return res.status(404).json({ message: "token not found" });
-    }
+export const logout = (_, res) => {
     res.clearCookie("token");
-    res.status(200).json({ message: "logout successfully" });
+    res.status(200).json({ message: "User logged out successfully" });
 };
 
-export { logOut, loginUser, RegisterUser };
+export const verifyEmail = async (req, res) => {
+    const { code } = req.body;
+    if (!code) return res.status(400).send("Please provide verification code");
+
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now() },
+        });
+        if (!user) {
+            return res.status(404).json({ message: "user not found" });
+        }
+        user.isverified = true;
+        user.verificationToken = undefined;
+        user.verificationTokenExpiresAt = undefined;
+
+        await user.save();
+        await sendWelcomeEmail(user.email, user.name);
+        res.status(200).json({ messgae: "you're verified successfully" });
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
+};
+
+export const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+    if (!email) {
+        return res
+            .status(404)
+            .json({ success: false, message: "email not found" });
+    }
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res
+                .status(404)
+                .json({ success: false, message: "user not found" });
+        }
+
+        //generate reset token
+        const resetToken = crypto.randomBytes(20).toString("hex");
+        const resetTokenExpiresAt = Date.now() + 1 * 60 * 60 * 1000;
+
+        user.resetPasswordToken = resetToken;
+        user.resetPasswordExpiresAt = resetTokenExpiresAt;
+
+        await user.save();
+
+        //send email
+
+        await sendPasswordResetEmail(
+            user.email,
+            `${process.env.CLIENT_URL}/reset-password/${resetToken}`
+        );
+
+        res.status(200).json({
+            success: true,
+            message: "password reset link has been sent to your email",
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ success: false, message: error.message });
+    }
+};
+
+export const resetPassword = async (req, res) => {
+    const { token } = req.params;
+    const { password } = req.body;
+    if (!token) {
+        return res
+            .status(404)
+            .json({ success: false, message: "token not found" });
+    }
+    const user = await User.findOne({
+        resetPasswordToken: token,
+        resetPasswordExpiresAt: { $gt: Date.now() },
+    });
+    if (!user) {
+        return res.status(404).json({ message: "user not found" });
+    }
+    //update password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpiresAt = undefined;
+    user.save();
+
+    await sendResetSuccessfulEmail(user.email);
+
+    res.status(200).json({
+        success: true,
+        message: "password reset successful",
+    });
+};
