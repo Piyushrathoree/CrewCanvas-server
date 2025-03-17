441d0ba067288fb74620e3b969178803f84ba798
Piyushrathoree <meonpath008@gmail.com>
2025-03-17 23:32:39 +0530

    Refactor app setup: add teamspace routes and adjust auth route path

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import router from "./routes/user.route.js";
-
+import teamspaceRouter from "./routes/teamspace.route.js";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(cookieParser());
+app.use(cookieParser()  );
 
 app.get("/", (_, res) => {
     res.send("hello , world");
 });
 
-app.use("/api/auth/", router);
-
+app.use("/api/auth", router);
+app.use('/api/teamspace', teamspaceRouter)
 export default app;
