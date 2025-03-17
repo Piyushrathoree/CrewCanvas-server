501e8ed6f43959decf16a6514cc8fabf0f6cb7df
Piyushrathoree <meonpath008@gmail.com>
2025-03-17 23:32:24 +0530

    Fix user model: change generateAuthToken from static to instance method

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-userSchema.statics.generateAuthToken = function () {
+userSchema.methods.generateAuthToken = function () {
     const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
         expiresIn: "7d",
     });
