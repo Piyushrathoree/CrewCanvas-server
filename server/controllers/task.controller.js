238c56a11ba10d59ed5be38cb932f44122242dd3
Piyushrathoree <meonpath008@gmail.com>
2025-03-19 16:22:16 +0530

    done with the whole backend api testing

--- server/controllers/task.controller.js
+++ server/controllers/task.controller.js
@@ -2,9 +2,9 @@ import Task from "../models/task.model.js";
 import Teamspace from "../models/teamspace.model.js";
 
 const createTask = async (req, res) => {
-    const { taskName, taskDescription, taskAssignedTo } = req.body;
+    const { taskName, taskDescription = "", taskAssignedTo } = req.body;
 
-    const taskAssignedBy = req.user._id;
+    const taskAssignedBy = req.user.id;
 
     const { teamspaceId } = req.params;
     if (!teamspaceId) {
@@ -14,7 +14,14 @@ const createTask = async (req, res) => {
     if (!currentTeamspace) {
         return res.status(404).json({ message: "Teamspace not found" });
     }
-
+    const isMember = currentTeamspace.members.find(
+        (member) => member.user.toString() === taskAssignedBy
+    );
+    if (!isMember || isMember.role !== "admin") {
+        return res
+            .status(401)
+            .json({ message: "current user is not a member of teamspace" });
+    }
     const taskExists = await Task.findOne({ taskName });
 
     if (taskExists) {
@@ -35,28 +42,37 @@ const createTask = async (req, res) => {
         return res.status(401).json({ message: "Error in creating a T sk" });
     }
     await task.save();
-    
+
     // Add note to teamspace's notes array
     await Teamspace.findByIdAndUpdate(teamspaceId, {
-        $push: { notes: note._id },
+        $push: { tasks: task._id },
     });
     res.status(201).json({ message: "Task Created successfully", task });
-};
+}; // checked
 
 const getTask = async (req, res) => {
     const { teamspaceId, taskId } = req.params;
 
-    if (!teamspaceId || !taskId) {
+    const userId = req.user.id
+
+    if (!teamspaceId || !taskId || !userId) {
         return res
             .status(404)
-            .json({ message: "Teamspace ID or Task ID not provided" });
+            .json({ message: "Teamspace ID or Task ID or userId not provided" });
     }
 
     const teamspace = await Teamspace.findById(teamspaceId);
     if (!teamspace) {
         return res.status(404).json({ message: "Teamspace not found" });
     }
-
+    const isMember = teamspace.members.find(
+        (member) => member.user.toString() === userId
+    );
+    if (!isMember || isMember.role !== "admin") {
+        return res
+            .status(401)
+            .json({ message: "current user is not a member of teamspace" });
+    }
     const task = await Task.findOne({
         _id: taskId,
         teamspaceId: teamspaceId,
@@ -69,34 +85,51 @@ const getTask = async (req, res) => {
     }
 
     res.status(200).json({ task });
-};
+}; // checked 
 
 const getTasksFromTeamspace = async (req, res) => {
     const { teamspaceId } = req.params;
-    if (!teamspaceId) {
-        return res.status(404).json({ message: "teamspace not found" });
+    const userId = req.user.id
+    if (!teamspaceId || !userId) {
+        return res.status(404).json({ message: "something is missing " });
     }
-    const currentTeamspace = await Teamspace.find({ _id: teamspaceId });
-    if (!currentTeamspace) {
-        return res.status(200).json({ message: "No notes added YET" });
+    const teamspace = await Teamspace.findById( teamspaceId );
+    if (!teamspace) {
+        return res.status(404).json({ message: "teamspace not found " });
+    }
+    const isMember = teamspace.members.find(
+        (member) => member.user.toString() === userId
+    );
+    if (!isMember || isMember.role !== "admin") {
+        return res
+            .status(401)
+            .json({ message: "current user is not a member of teamspace" });
     }
 
-    res.status(200).json({ tasks: currentTeamspace.tasks });
-};
+    res.status(200).json({ tasks: teamspace.tasks });
+};  // checked 
 
 const updateTask = async (req, res) => {
     const { teamspaceId, taskId } = req.params;
     const { taskName, taskDescription } = req.body;
-
-    if (!teamspaceId || !taskId) {
+    const userId = req.user.id
+    if (!teamspaceId || !taskId || !userId) {
         return res
             .status(404)
-            .json({ message: "Teamspace ID or Task ID not provided" });
+            .json({ message: "Teamspace ID or Task ID or User ID not provided" });
     }
 
     const teamspace = await Teamspace.findById(teamspaceId);
     if (!teamspace) {
         return res.status(404).json({ message: "Teamspace not found" });
+    }
+    const isMember = teamspace.members.find(
+        (member) => member.user.toString() === userId
+    );
+    if (!isMember || isMember.role !== "admin") {
+        return res
+            .status(401)
+            .json({ message: "current user is not a member of teamspace" });
     }
 
     if (!taskName || !taskDescription) {
@@ -109,7 +142,7 @@ const updateTask = async (req, res) => {
         {
             _id: taskId,
             teamspaceId: teamspaceId,
-            taskAssignedBy: req.user._id,
+            taskAssignedBy: userId,
         },
         { taskName, taskDescription },
         { new: true }
@@ -122,20 +155,29 @@ const updateTask = async (req, res) => {
     }
 
     res.status(200).json({ message: "Task Updated Successfully", task });
-};
+}; // checked   
 
 const deleteTask = async (req, res) => {
     const { teamspaceId, taskId } = req.params;
+    const userId = req.user.id
 
-    if (!teamspaceId || !taskId) {
+    if (!teamspaceId || !taskId || !userId) {
         return res
             .status(404)
-            .json({ message: "Teamspace ID or Task ID not provided" });
+            .json({ message: "Teamspace ID or Task ID or user ID not provided" });
     }
 
     const teamspace = await Teamspace.findById(teamspaceId);
     if (!teamspace) {
         return res.status(404).json({ message: "Teamspace not found" });
+    }
+    const isMember = teamspace.members.find(
+        (member) => member.user.toString() === userId
+    );
+    if (!isMember || isMember.role !== "admin") {
+        return res
+            .status(401)
+            .json({ message: "current user is not a member of teamspace" });
     }
 
     const deletedTask = await Task.findOneAndDelete({
@@ -152,13 +194,13 @@ const deleteTask = async (req, res) => {
     // Remove task reference from teamspace
     await Teamspace.findByIdAndUpdate(teamspaceId, {
         $pull: { tasks: taskId },
-    });
+            });
 
     res.status(200).json({
         message: "Task successfully deleted",
         deletedTask,
     });
-};
+}; // checked 
 
 const toggleTaskStatus = async (req, res) => {
     const { teamspaceId, taskId } = req.params;
@@ -172,6 +214,13 @@ const toggleTaskStatus = async (req, res) => {
     const teamspace = await Teamspace.findById(teamspaceId);
     if (!teamspace) {
         return res.status(404).json({ message: "Teamspace not found" });
+    }
+
+    const isMember = teamspace.members.some(
+        (member) => member.user.toString() === req.user.id
+    );
+    if (!isMember){
+        return res.status(401).json({message:"you're not a part to this teamspace"})
     }
 
     const task = await Task.findOne({ _id: taskId, teamspaceId });
@@ -192,7 +241,7 @@ const toggleTaskStatus = async (req, res) => {
         message: "Task status updated successfully",
         task: updatedTask,
     });
-};
+}; // checked 
 
 export {
     createTask,
