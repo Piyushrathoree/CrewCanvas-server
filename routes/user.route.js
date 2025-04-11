2a4e13db1c55eb79fdc5195b1084187b39315df8
Piyushrathoree <meonpath008@gmail.com>
2025-04-12 00:36:45 +0530

    refactor: add comments to clarify purpose of authentication routes in user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -24,14 +24,14 @@ const router = Router();
 // ✅ Normal Authentication Routes
 router.post("/signup", registerUser);
 router.post("/login", loginUser);
-router.post("/logout", logout);
+router.post("/logout", logout); // --> dashboard in frontend
 
-router.post("/verify-email", verifyEmail);
-router.post("/forgot-password", forgotPassword);
-router.post("/reset-password/:token", resetPassword);
+router.post("/verify-email", verifyEmail); //--> verify page
+router.post("/forgot-password", forgotPassword); // --> forgot password page
+router.post("/reset-password/:token", resetPassword); // --> reset password page
 router.get("/protected", verifyJWT, (_, res) => {
     res.send(
         '<div style="background-color: black; color: white; height: 100vh; display: flex; justify-content: center; align-items: center;"><h1>your freaking middleware is working totally fine 😂☠️</h1> </div>'
     );
-});
+})
 export default router;
